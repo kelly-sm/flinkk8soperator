dc7506d8a5cccc70f1c5f779e2e1ff830ae6124e
Kelly Smith <kellysm@zillowgroup.com>
2020-01-13 15:41:39 -0800

    Updating the CRD to enable pod security context to be specified and applied to JM and TM pods.

--- pkg/controller/flink/task_manager_controller.go
+++ pkg/controller/flink/task_manager_controller.go
@@ -209,6 +209,7 @@ func taskmanagerTemplate(app *v1beta1.FlinkApplication) *v1.Deployment {
 					Volumes:          app.Spec.Volumes,
 					ImagePullSecrets: app.Spec.ImagePullSecrets,
 					NodeSelector:     app.Spec.TaskManagerConfig.NodeSelector,
+					SecurityContext: 	&app.Spec.SecurityContext,
 				},
 			},
 		},
