31512a9f531dc5022574b1f4218f9ca07fed6ccd
Kelly Smith <kellysm@zillowgroup.com>
2020-01-13 18:01:41 -0800

    Fixing spacing, adding security context fields to crd validation.

--- pkg/controller/flink/job_manager_controller.go
+++ pkg/controller/flink/job_manager_controller.go
@@ -333,7 +333,7 @@ func jobmanagerTemplate(app *v1beta1.FlinkApplication) *v1.Deployment {
 					Volumes:          app.Spec.Volumes,
 					ImagePullSecrets: app.Spec.ImagePullSecrets,
 					NodeSelector:     app.Spec.JobManagerConfig.NodeSelector,
-					SecurityContext: 	&app.Spec.SecurityContext,
+					SecurityContext:  &app.Spec.SecurityContext,
 				},
 			},
 		},
